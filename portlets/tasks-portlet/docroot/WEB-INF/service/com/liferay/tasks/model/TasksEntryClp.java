b874587aa178cbeeaf7d59b9634f76bdc8eddeba
Evan Thibodeau <evan.thibodeau@liferay.com>
2013-01-18 10:20:37 -0800

    SOS-1376 Regenerate

--- portlets/tasks-portlet/docroot/WEB-INF/service/com/liferay/tasks/model/TasksEntryClp.java
+++ portlets/tasks-portlet/docroot/WEB-INF/service/com/liferay/tasks/model/TasksEntryClp.java
@@ -56,7 +56,7 @@ public class TasksEntryClp extends BaseModelImpl<TasksEntry>
 	}
 
 	public Serializable getPrimaryKeyObj() {
-		return _tasksEntryId;
+		return new Long(_tasksEntryId);
 	}
 
 	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
