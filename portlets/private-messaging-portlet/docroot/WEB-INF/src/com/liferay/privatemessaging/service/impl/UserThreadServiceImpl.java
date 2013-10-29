449f57ffc59f1fd5d1f4ff52ac4dd02bdc6358db
Bruno Farache <bruno.farache@liferay.com>
2013-10-29 14:02:32 -0700

    autogenerated

---
+++ portlets/private-messaging-portlet/docroot/WEB-INF/src/com/liferay/privatemessaging/service/impl/UserThreadServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.privatemessaging.service.impl;
+
+import com.liferay.privatemessaging.service.base.UserThreadServiceBaseImpl;
+
+/**
+ * The implementation of the user thread remote service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.privatemessaging.service.UserThreadService} interface.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see com.liferay.privatemessaging.service.base.UserThreadServiceBaseImpl
+ * @see com.liferay.privatemessaging.service.UserThreadServiceUtil
+ */
+public class UserThreadServiceImpl extends UserThreadServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this interface directly. Always use {@link com.liferay.privatemessaging.service.UserThreadServiceUtil} to access the user thread remote service.
+	 */
+}
