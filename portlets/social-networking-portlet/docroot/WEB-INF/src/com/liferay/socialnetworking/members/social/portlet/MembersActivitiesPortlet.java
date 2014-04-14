b18be99afc676cd47ac38ca24bc9a125fe5f2c72
dale <dale.shan@liferay.com>
2014-04-14 17:19:16 +0800

    LPS-45608 Copy some logic from com.liferay.portlet.activities.action.RSSAction to add a new MembersActivitiesPortlet portlet

---
+++ portlets/social-networking-portlet/docroot/WEB-INF/src/com/liferay/socialnetworking/members/social/portlet/MembersActivitiesPortlet.java
@@ -0,0 +1,246 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.socialnetworking.members.social.portlet;
+
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.Portal;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityFeedEntry;
+import com.liferay.portlet.social.service.SocialActivityInterpreterLocalServiceUtil;
+import com.liferay.portlet.social.service.SocialActivityLocalServiceUtil;
+import com.liferay.util.RSSUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndContentImpl;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.feed.synd.SyndFeedImpl;
+import com.sun.syndication.feed.synd.SyndLink;
+import com.sun.syndication.feed.synd.SyndLinkImpl;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+/**
+ * @author Dale Shan
+ */
+public class MembersActivitiesPortlet extends MVCPortlet {
+
+	@Override
+	public void serveResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws IOException, PortletException {
+
+		OutputStream outputStream = null;
+
+		try {
+			if (!PortalUtil.isRSSFeedsEnabled()) {
+				PortalUtil.sendRSSFeedsDisabledError(
+					resourceRequest, resourceResponse);
+
+				return;
+			}
+
+			resourceResponse.setContentType(ContentTypes.TEXT_XML_UTF8);
+
+			outputStream = resourceResponse.getPortletOutputStream();
+
+			byte[] bytes = getRSS(resourceRequest, resourceResponse);
+
+			outputStream.write(bytes);
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+		finally {
+			outputStream.close();
+		}
+	}
+
+	protected String exportToRSS(
+			PortletRequest portletRequest, String title, String description,
+			String format, double version, String displayStyle,
+			List<SocialActivity> activities, ServiceContext serviceContext)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		SyndFeed syndFeed = new SyndFeedImpl();
+
+		syndFeed.setDescription(GetterUtil.getString(description, title));
+
+		List<SyndEntry> syndEntries = new ArrayList<SyndEntry>();
+
+		syndFeed.setEntries(syndEntries);
+
+		for (SocialActivity activity : activities) {
+			SocialActivityFeedEntry activityFeedEntry =
+				SocialActivityInterpreterLocalServiceUtil.interpret(
+					StringPool.BLANK, activity, serviceContext);
+
+			if (activityFeedEntry == null) {
+				continue;
+			}
+
+			SyndEntry syndEntry = new SyndEntryImpl();
+
+			SyndContent syndContent = new SyndContentImpl();
+
+			syndContent.setType(RSSUtil.ENTRY_TYPE_DEFAULT);
+
+			String value = null;
+
+			if (displayStyle.equals(RSSUtil.DISPLAY_STYLE_TITLE)) {
+				value = StringPool.BLANK;
+			}
+			else {
+				value = activityFeedEntry.getBody();
+			}
+
+			syndContent.setValue(value);
+
+			syndEntry.setDescription(syndContent);
+
+			if (Validator.isNotNull(activityFeedEntry.getLink())) {
+				syndEntry.setLink(activityFeedEntry.getLink());
+			}
+
+			syndEntry.setPublishedDate(new Date(activity.getCreateDate()));
+			syndEntry.setTitle(
+				HtmlUtil.extractText(activityFeedEntry.getTitle()));
+			syndEntry.setUri(syndEntry.getLink());
+
+			syndEntries.add(syndEntry);
+		}
+
+		syndFeed.setFeedType(RSSUtil.getFeedType(format, version));
+
+		List<SyndLink> syndLinks = new ArrayList<SyndLink>();
+
+		syndFeed.setLinks(syndLinks);
+
+		SyndLink selfSyndLink = new SyndLinkImpl();
+
+		syndLinks.add(selfSyndLink);
+
+		String link =
+			PortalUtil.getLayoutFullURL(themeDisplay) +
+				Portal.FRIENDLY_URL_SEPARATOR + "members_activities/rss";
+
+		selfSyndLink.setHref(link);
+
+		selfSyndLink.setRel("self");
+
+		SyndLink alternateSyndLink = new SyndLinkImpl();
+
+		syndLinks.add(alternateSyndLink);
+
+		alternateSyndLink.setHref(PortalUtil.getLayoutFullURL(themeDisplay));
+		alternateSyndLink.setRel("alternate");
+
+		syndFeed.setPublishedDate(new Date());
+		syndFeed.setTitle(title);
+		syndFeed.setUri(link);
+
+		return RSSUtil.export(syndFeed);
+	}
+
+	protected List<SocialActivity> getActivities(
+			PortletRequest portletRequest, int max)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Group group = GroupLocalServiceUtil.getGroup(
+			themeDisplay.getScopeGroupId());
+
+		int start = 0;
+
+		if (group.isOrganization()) {
+			return SocialActivityLocalServiceUtil.getOrganizationActivities(
+				group.getOrganizationId(), start, max);
+		}
+		else if (group.isRegularSite()) {
+			return SocialActivityLocalServiceUtil.getGroupActivities(
+				group.getGroupId(), start, max);
+		}
+		else if (group.isUser()) {
+			return SocialActivityLocalServiceUtil.getUserActivities(
+				group.getClassPK(), start, max);
+		}
+
+		return Collections.emptyList();
+	}
+
+	protected byte[] getRSS(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		String feedTitle = ParamUtil.getString(resourceRequest, "feedTitle");
+		String type = ParamUtil.getString(
+			resourceRequest, "type", RSSUtil.FORMAT_DEFAULT);
+		double version = ParamUtil.getDouble(
+			resourceRequest, "version", RSSUtil.VERSION_DEFAULT);
+		String displayStyle = ParamUtil.getString(
+			resourceRequest, "displayStyle", RSSUtil.DISPLAY_STYLE_DEFAULT);
+
+		int max = ParamUtil.getInteger(
+			resourceRequest, "max", SearchContainer.DEFAULT_DELTA);
+
+		List<SocialActivity> activities = getActivities(resourceRequest, max);
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			resourceRequest);
+
+		String rss = exportToRSS(
+			resourceRequest, feedTitle, null, type, version, displayStyle,
+			activities, serviceContext);
+
+		return rss.getBytes(StringPool.UTF8);
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		MembersActivitiesPortlet.class);
+
+}
